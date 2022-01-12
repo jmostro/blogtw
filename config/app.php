d06200e4745836ff13b0d46276191ed1237259e8
jmostro <javiermondini@gmail.com>
2022-01-12 15:14:30 -0300

    Added user and entry factories, added guestcontroller for auth routing. Added first localization texts

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'BlogTwitt'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'development'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://blogtw.test'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -106,7 +106,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'es_AR',
 
     /*
     |--------------------------------------------------------------------------
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -12,7 +12,23 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
+    'Login' => 'Login',
+    'Email' => 'E-Mail Address',
+    'Password' => 'Password',
+    'Password_Confirm' => 'Confirm password',
+    'Reset_Password' => 'Reset password',
+    'Send_Reset_Email' => 'Send password reset link e-mail',
+    'auth.Please_Confirm_Password' => 'Please confirm your password to continue',
+    'Remember' => 'Remember me',
+    'Forgot' => 'Forgot your password?',
+    'Name' => 'Name',
+    'Register' => 'Register',
+    'Verify_Mail' => 'Verify your mail address',
+    'Verify_Mail_Resent' => 'A fresh verification link has been sent to your email address',
+    'Check_Mail_Proceding' => 'Before proceeding, please check your email for a verification link.',
+    'Not_Received_Email' => 'If you not received the email',
+    'Request_Another_Verify' => 'Click here to request another',
+    'LoggedIn' => 'You are logged in!',
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
